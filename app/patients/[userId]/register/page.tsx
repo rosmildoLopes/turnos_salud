6ba2877cbce22d8965aa0246b4fe0b8bb93d0ece
rosmildoLopes <[200~123316625+rosmildoLopes@users.noreply.github.com~>
2024-07-29 02:02:05 -0300

    crear nuevo usuario exitosamente

---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,36 @@
+import { RegisterForm } from '@/components/forms/RegisterForm'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+      {/* ottp verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo patient"
+            className="h-10 w-fit mb-12"
+          />
+          <RegisterForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left ">
+              © 2024 CarePulse
+            </p>
+            <Link href='/?admin=true' className="text-green-500">Admin</Link>
+          </div>
+        </div>
+      </section><Image src='/assets/images/register-img.png' 
+      width={1000}
+      height={1000}
+      alt="patient"
+      className="side-img max-w-[390px]"/>
+    </div>
+  )
+}
+
+export default Register
